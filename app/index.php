d9a0e2987bdd2f45b30a97b310596886d3eb790c
Simon Jensen <admin@simon-jensen.net>
2019-03-18 19:09:28 +0100

    A simple information page added

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h3>Hello from from the other side!</h3>
+    <p>
+      Pro tip: Check the response header <code>X-Cache</code> to see if this page was delivered from Varnish or the nginx backend.
+    </p>
+  </body>
+</html>
